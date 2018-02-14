a4e68dcfd4282c9c9717d9e74f677cd815fd0509
Areii1 <ari.jaaskelainen1@gmail.com>
2018-02-14 03:17:04 +0200

    api movie search works

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import apikey from '../apikey';
+import axios from 'axios';
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       searchTerm: '',
+      movieListResponse: [],
     }
   }
 
@@ -12,12 +15,30 @@ class App extends Component {
     this.setState({searchTerm: e.target.value});
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    axios({
+      method:'get',
+      
+      url:'https://api.themoviedb.org/3/search/movie?api_key=' + 
+      apikey + 
+      '&language=en-US&query=' + 
+      this.state.searchTerm + 
+      '&page=1&include_adult=true',
+    })
+      .then(function(response) {
+        this.setState({movieListResponse: response.data.results});
+      });
+  }
+
   render() {
     return (
       <div>
         <h1>movie-finder</h1>
-        <input className="searchbar" 
-          onChange={(event) => this.handleSearchBarChange(event)}></input>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input className="searchbar" 
+            onChange={(event) => this.handleSearchBarChange(event)}></input>
+        </form>
         <p>{this.state.searchTerm}</p>
       </div>
     )
