0d5a85ed0a1dfd669a508cdd757cf948bb056d8e
Areii1 <ari.jaaskelainen1@gmail.com>
2018-02-18 23:33:32 +0200

    render picture in movieView

--- src/components/MovieView.jsx
+++ src/components/MovieView.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import apikey from '../apikey';
 
-function MovieView(props) {
-  return (
-    <div>
-      <p> MOvie : {props.match.params.id} </p>
-      <Link to="/">back to the main page </Link>
-    </div>
-  );
+const language = 'en-us';
+
+class MovieView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movieDetails: [],
+    };
+  }
+
+  componentWillMount() {
+    const movieId = this.props.match.params.id;
+    axios({
+      method: 'get',
+      url: `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apikey}&language=${language}`,
+    }).then((response) => {
+      this.setState({ movieDetails: response.data });
+    });
+  }
+
+  render() {
+    const backdropUrl = 'https://image.tmdb.org/t/p/original';
+    console.log(backdropUrl + this.state.movieDetails.backdrop_path);
+    return (
+      <div>
+        <p> MOvie : {this.props.match.params.id} </p>
+        {this.state.movieDetails && (
+          <img src={backdropUrl + this.state.movieDetails.backdrop_path} alt="Movie header" />
+        )}
+        <Link to="/">back to the main page </Link>
+      </div>
+    );
+  }
 }
 
 MovieView.propTypes = {
