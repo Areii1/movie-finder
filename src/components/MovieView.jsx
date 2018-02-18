a4daedeaf6bb872f309acfef0a45f93a45c2f54f
Areii1 <ari.jaaskelainen1@gmail.com>
2018-02-19 00:30:50 +0200

    add linear gradient to movieView header img

--- src/components/MovieView.jsx
+++ src/components/MovieView.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import apikey from '../apikey';
+import './MovieView.css';
 
 const language = 'en-us';
 
@@ -11,7 +12,7 @@ class MovieView extends Component {
     super(props);
 
     this.state = {
-      movieDetails: [],
+      movieDetails: null,
     };
   }
 
@@ -27,13 +28,19 @@ class MovieView extends Component {
 
   render() {
     const backdropUrl = 'https://image.tmdb.org/t/p/original';
-    console.log(backdropUrl + this.state.movieDetails.backdrop_path);
     return (
       <div>
-        <p> MOvie : {this.props.match.params.id} </p>
-        {this.state.movieDetails && (
-          <img src={backdropUrl + this.state.movieDetails.backdrop_path} alt="Movie header" />
-        )}
+        { this.state.movieDetails ?
+          <div className="content-wrapper">
+            <div className="header-img" style={{ background: `url(${backdropUrl + this.state.movieDetails.backdrop_path}) center/cover no-repeat` }} />
+            <div className="text-content-wrapper">
+              <p className="movie-description">
+                {this.state.movieDetails.overview}
+              </p>
+            </div>
+          </div>
+        : (<p>...</p>)
+        }
         <Link to="/">back to the main page </Link>
       </div>
     );
