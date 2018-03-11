9491985dfa5e5f850695e31254c96b4cc976ddad
Areii1 <ari.jaaskelainen1@gmail.com>
2018-03-11 18:16:00 +0200

    movie information works, gradient needs to be replaced

--- src/components/MovieView.jsx
+++ src/components/MovieView.jsx
@@ -13,6 +13,7 @@ class MovieView extends Component {
 
     this.state = {
       movieDetails: null,
+      movieCredits: null,
     };
   }
 
@@ -24,36 +25,108 @@ class MovieView extends Component {
     }).then((response) => {
       this.setState({ movieDetails: response.data });
     });
+
+    axios({
+      method: 'get',
+      url: `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apikey}`,
+    }).then((response) => {
+      this.setState({ movieCredits: response.data });
+    });
   }
+
+  getDirector() {
+    const director = this.state.movieCredits.crew.filter((member) => {
+      if (member.job === 'Director') {
+        return member;
+      }
+      return null;
+    });
+    console.log(director[0].name);
+    return director[0].name;
+  }
+
 
   render() {
     const backdropUrl = 'https://image.tmdb.org/t/p/original';
     return (
       <div>
-        { this.state.movieDetails ?
+        { this.state.movieDetails && this.state.movieCredits ?
           <div className="content-wrapper">
             <div className="img-section-wrapper" style={{ background: `url(${backdropUrl + this.state.movieDetails.backdrop_path}) center/cover no-repeat` }}>
-              <div className="img-section-gradient" >
-                <div className="content-container">
-                  <p className="movie-title">
-                    {this.state.movieDetails.title}
-                  </p>
-                  <p className="movie-release-date-label">
-                    Release date:
-                  </p>
-                  <p className="movie-release-date">
-                    {this.state.movieDetails.release_date}
-                  </p>
+              <div className="img-section-content-container">
+                <h1 className="headline" >
+                  Movie-finder
+                </h1>
+                <p className="movie-title">
+                  {this.state.movieDetails.title}
+                </p>
+                <div className="img-section-movie-info">
+                  <div id="movie-release-date-container">
+                    <p
+                      className="movie-info-label"
+                      id="movie-release-date-label"
+                    >
+                      Release date:
+                    </p>
+                    <p
+                      className="movie-info-item"
+                      id="movie-release-date"
+                    >
+                      {this.state.movieDetails.release_date}
+                    </p>
+                  </div>
+                  <div id="movie-director-info">
+                    <p
+                      className="movie-info-label"
+                      id="movie-director-label"
+                    >
+                      director:
+                    </p>
+                    <p
+                      className="movie-info-item"
+                      id="movie-director"
+                    >
+                      {this.getDirector()}
+                    </p>
+                  </div>
+                  <div id="movie-genre-info">
+                    <p
+                      className="movie-info-label"
+                      id="movie-genre-label"
+                    >
+                      genres:
+                    </p>
+                    <p
+                      className="movie-info-item"
+                      id="movie-genres"
+                    >
+                      drama + blaa blaa
+                    </p>
+                  </div>
+                  <div id="movie-poster-info">
+                    <p
+                      className="movie-info-label"
+                      id="movie-poster-label"
+                    >
+                      poster:
+                    </p>
+                    <p
+                      className="movie-info-item"
+                      id="movie-poster"
+                    >
+                      <img
+                        className="poster"
+                        src={backdropUrl + this.state.movieDetails.poster_path}
+                        alt="Movie poster"
+                      />
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="content-container">
               <p className="movie-overview"> {this.state.movieDetails.overview} </p>
-              <img
-                className="poster"
-                src={backdropUrl + this.state.movieDetails.poster_path}
-                alt="Movie poster"
-              />
+              
               <Link to="/">back to the main page </Link>
             </div>
           </div>
