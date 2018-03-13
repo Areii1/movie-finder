1970aeb02d055199eb6db2d076c34c8436c6f1c7
Areii1 <ari.jaaskelainen1@gmail.com>
2018-03-13 17:59:36 +0200

    add box shadow to poster and proptypes to cast list component

--- src/components/CastMembersList.jsx
+++ src/components/CastMembersList.jsx
@@ -6,25 +6,22 @@ import QuestionMark from '../media/question-mark.png';
 const urlStart = 'https://image.tmdb.org/t/p/w92';
 
 function CastMembersList(props) {
-  console.log(props.movieDetails);
-  const list = props.movieDetails.credits.cast.map((member, index) =>
-    (
-      <li
-        className={(index % 2 === 0) ? 'even' : 'odd'}
-        key={member.credit_id}
-      >
-        <img
-          className="cast-member-picture"
-          src={(member.profile_path) ? urlStart + member.profile_path : QuestionMark}
-          alt={member.name}
-        />
+  const list = props.movieDetails.credits.cast.map((member, index) => (
+    <li
+      className={(index % 2 === 0) ? 'even' : 'odd'}
+      key={member.credit_id}
+    >
+      <img
+        className="cast-member-picture"
+        src={(member.profile_path) ? urlStart + member.profile_path : QuestionMark}
+        alt={member.name}
+      />
 
-        <p className="cast-member-name">{member.name}</p>
-        <p className="appearing-as-label">Appearing as</p>
-        <p className="cast-member-character">{member.character}</p>
-      </li>
-    ),
-  );
+      <p className="cast-member-name">{member.name}</p>
+      <p className="appearing-as-label">Appearing as</p>
+      <p className="cast-member-character">{member.character}</p>
+    </li>
+  ));
 
   return (
     <ul>
@@ -33,8 +30,8 @@ function CastMembersList(props) {
   );
 }
 
-// CastMembersList.propTypes = {
-//   movieDetails: PropTypes.objectOf(),
-// };
+CastMembersList.propTypes = {
+  movieDetails: PropTypes.objectof(PropTypes.object).isRequired,
+};
 
 export default CastMembersList;
