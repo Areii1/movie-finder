3d8ff25ca088b2d24d6d6ae9674e414fad99e36d
Areii1 <ari.jaaskelainen1@gmail.com>
2018-03-13 17:20:44 +0200

    add profile pictures to cast list

--- src/components/CastMembersList.jsx
+++ src/components/CastMembersList.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './CastMembersList.css';
 
+const urlStart = 'https://image.tmdb.org/t/p/original';
 
 function CastMembersList(props) {
+  console.log(props.movieDetails);
   const list = props.movieDetails.credits.cast.map((member, index) =>
     (
       <li
         className={(index % 2 === 0) ? 'even' : 'odd'}
         key={member.credit_id}
       >
+        <img
+          className="cast-member-picture"
+          src={urlStart + member.profile_path}
+          alt={member.name}
+        />
+
         <p className="cast-member-name">{member.name}</p>
         <p className="appearing-as-label">Appearing as</p>
         <p className="cast-member-character">{member.character}</p>
