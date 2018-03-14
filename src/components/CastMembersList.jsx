fdf1cce25c1ce854a340d479f202117813e1a390
Areii1 <ari.jaaskelainen1@gmail.com>
2018-03-14 17:24:25 +0200

    limit cast member list to only 15 items

--- src/components/CastMembersList.jsx
+++ src/components/CastMembersList.jsx
@@ -6,22 +6,25 @@ import QuestionMark from '../media/question-mark.png';
 const urlStart = 'https://image.tmdb.org/t/p/w92';
 
 function CastMembersList(props) {
-  const list = props.movieDetails.credits.cast.map((member, index) => (
-    <li
-      className={(index % 2 === 0) ? 'even' : 'odd'}
-      key={member.credit_id}
-    >
-      <img
-        className="cast-member-picture"
-        src={(member.profile_path) ? urlStart + member.profile_path : QuestionMark}
-        alt={member.name}
-      />
-
-      <p className="cast-member-name">{member.name}</p>
-      <p className="appearing-as-label">Appearing as</p>
-      <p className="cast-member-character">{member.character}</p>
-    </li>
-  ));
+  const list = props.movieDetails.credits.cast.map((member, index) => {
+    if (index < 15) {
+      return (
+        <li
+          className={(index % 2 === 0) ? 'even' : 'odd'}
+          key={member.credit_id}
+        >
+          <img
+            className="cast-member-picture"
+            src={(member.profile_path) ? urlStart + member.profile_path : QuestionMark}
+            alt={member.name}
+          />
+          <p className="cast-member-name">{member.name}</p>
+          <p className="appearing-as-label">Appearing as</p>
+          <p className="cast-member-character">{member.character}</p>
+        </li>
+      );
+    }
+  });
 
   return (
     <ul>
