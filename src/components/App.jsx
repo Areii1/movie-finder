d2ad4921703094ceace64956066580ce2e7f7871
Areii1 <ari.jaaskelainen1@gmail.com>
2018-03-18 17:59:22 +0200

    add basename to Router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import './Global.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <div className="app-router-wrapper">
         <Route exact path="/" component={MainView} />
         <Route path="/movieview/:id" component={MovieView} />
