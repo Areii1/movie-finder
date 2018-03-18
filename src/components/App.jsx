e0166851eac170f9fd698e82e71c1212e138d649
Areii1 <ari.jaaskelainen1@gmail.com>
2018-03-18 18:26:10 +0200

    change browserRouter to HashRouter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import MainView from './MainView';
 import MovieView from './MovieView';
 import './Global.css';
 
 function App() {
   return (
-    <Router basename="/">
+    <Router>
       <div className="app-router-wrapper">
         <Route exact path="/" component={MainView} />
         <Route path="/movieview/:id" component={MovieView} />
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((<App />), document.getElementById('root'));
