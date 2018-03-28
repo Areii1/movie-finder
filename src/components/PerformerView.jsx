1475bceaa6314a55809c9837f78baa0083d317ef
Areii1 <ari.jaaskelainen1@gmail.com>
2018-03-28 18:55:45 +0300

    add performer image and overview

--- src/components/PerformerView.jsx
+++ src/components/PerformerView.jsx
@@ -25,11 +25,45 @@ class PerformerView extends Component {
   }
 
   render() {
+    const backdropHeaderUrl = 'https://image.tmdb.org/t/p/original';
+
+    console.log(this.state.performerInfo, 'info');
     return (
       <div>
-        {this.state.performerInfo && (
-          <p>{this.state.performerInfo.name} is a {this.state.performerInfo.gender === 2 ? 'boy' : 'girl'}</p>
-        )}
+        { this.state.performerInfo ?
+          <div className="performer-view-content-wrapper">
+            <div className="performer-img-section-wrapper" style={{ background: `url(${backdropHeaderUrl + this.state.performerInfo.profile_path}) center/cover no-repeat` }}>
+              <div className="performer-img-section-gradient" >
+                <div className="container performer-img-section-content-container" >
+                  <h1 className="performer-headline" >
+                    MOVIE-FINDER
+                  </h1>
+                  <p className="performer-name">
+                    {this.state.performerInfo.name ?
+                    this.state.performerInfo.name : '...'}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="container content-container">
+              <div className="performer-biography-item">
+                <h2 className="performer-biography-label">
+                  BIOGRAPHY
+                </h2>
+                <p className="performer-biography"> {this.state.performerInfo.biography} </p>
+              </div>
+              {/* <div className="information">
+                <h2 className="information-label">INFORMATION</h2>
+                <div>
+                  <p>runtime: {`${this.state.movieDetails.runtime} minutes`}</p>
+                  <p>rating: {this.state.movieDetails.vote_average}</p>
+                  <p>budget: {`${this.state.movieDetails.budget} $`}</p>
+                </div>
+              </div> */}
+            </div>
+          </div>
+        : (<p className="performer-loading">LOADING</p>)
+        }
       </div>
     );
   }
